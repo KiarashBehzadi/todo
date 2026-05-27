999ad4df5b54df984cbef4d43dbc9e27e80d4e38
Kiarash Behzadi <kiarashbehzadi79@gmail.com>
2026-05-27 12:21:05 +0330

    feat: add TodoRepository with findbyuserId method

---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,17 @@
+package com.example.todo.repository;
+
+import com.example.todo.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    @Query("select t from Todo t where t.user.userId = :userId")
+    List<Todo> findByUserId(@Param("userId") Long userId);
+
+}
