4919a0c1ed3891fd16633bd1112ac542b1dab13c
Kiarash Behzadi <kiarashbehzadi79@gmail.com>
2026-05-27 12:17:45 +0330

    refactor: long datatype to Long userId

--- src/main/java/com/example/todo/entity/User.java
+++ src/main/java/com/example/todo/entity/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long userId;
+    private Long userId;
 
     @Column(nullable = false)
     private String username;
