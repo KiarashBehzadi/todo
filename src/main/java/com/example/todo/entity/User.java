8b33677bba6766bd8ec9223c9b4440653624176d
Kiarash Behzadi <kiarashbehzadi79@gmail.com>
2026-05-27 10:56:39 +0330

    feat: add getter,setter,const lombok

--- src/main/java/com/example/todo/entity/User.java
+++ src/main/java/com/example/todo/entity/User.java
@@ -1,9 +1,19 @@
 package com.example.todo.entity;
 
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
+@Entity
+@Table(name = "Tbl_user")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
 
