b1bf6567a86d14d55f249a1a3fda5f29b73bd1d7
Kiarash Behzadi <kiarashbehzadi79@gmail.com>
2026-05-28 12:25:39 +0330

    feat: add @data and constructors in UserRequestDto

--- src/main/java/com/example/todo/dto/UserRequestDto.java
+++ src/main/java/com/example/todo/dto/UserRequestDto.java
@@ -1,7 +1,14 @@
 package com.example.todo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRequestDto {
 
-    private Long username;
+    private String username;
     private String password;
 }
