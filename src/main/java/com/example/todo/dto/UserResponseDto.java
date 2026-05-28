4c58de5ac6f7e3a81b73aa694b4b3a8a6d42c029
Kiarash Behzadi <kiarashbehzadi79@gmail.com>
2026-05-28 12:25:25 +0330

    feat: add @data and constructors in UserResponseDto

--- src/main/java/com/example/todo/dto/UserResponseDto.java
+++ src/main/java/com/example/todo/dto/UserResponseDto.java
@@ -1,5 +1,12 @@
 package com.example.todo.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserResponseDto {
 
     private Long userId;
