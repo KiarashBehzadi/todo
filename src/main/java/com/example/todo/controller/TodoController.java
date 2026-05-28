592d962b48d010ead5bd040e2fe0f6bf69b020d1
Kiarash Behzadi <kiarashbehzadi79@gmail.com>
2026-05-28 12:35:24 +0330

    feat: add TodoController with implementations of POST, GET methods

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,34 @@
+package com.example.todo.controller;
+
+import com.example.todo.dto.TodoRequestDto;
+import com.example.todo.dto.TodoResponseDto;
+import com.example.todo.service.TodoService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users/{userId}/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+
+    @PostMapping
+    public TodoResponseDto createTodo(@PathVariable Long userId,
+                                      @RequestBody @Valid TodoRequestDto dto) {
+        return todoService.createTodo(userId, dto);
+    }
+
+    @GetMapping
+    public List<TodoResponseDto> getTodos(@PathVariable Long userId){
+        return todoService.getUserTodos(userId);
+    }
+
+
+}
