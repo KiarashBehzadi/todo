7ff7e864572a24193ba81ab3f613a8b59d5872f8
Kiarash Behzadi <kiarashbehzadi79@gmail.com>
2026-05-28 12:36:19 +0330

    feat: add UserController with POST method in /controller

---
+++ src/main/java/com/example/todo/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.example.todo.controller;
+
+import com.example.todo.dto.UserRequestDto;
+import com.example.todo.dto.UserResponseDto;
+import com.example.todo.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @PostMapping
+    public UserResponseDto createUser(@RequestBody UserRequestDto userRequestDto) {
+        return userService.createUser(userRequestDto);
+    }
+
+}
